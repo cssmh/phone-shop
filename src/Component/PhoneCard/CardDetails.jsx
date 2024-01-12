457393b948556ba0829560793712cabd037def2c
momen210 <touristmomen@gmail.com>
2024-01-12 21:38:03 +0600

    phone details left to make mobile responsive, its complete

--- src/Component/PhoneCard/CardDetails.jsx
+++ src/Component/PhoneCard/CardDetails.jsx
@@ -32,15 +32,15 @@ const CardDetails = () => {
           Phone details
         </h1>
       </div>
-      <div className="rounded-lg shadow-xl flex justify-center w-3/5 mx-auto items-center py-2 m-6 gap-4">
-        <img className="h-64 pb-5" src={image} alt="product image" />
+      <div className="rounded-lg shadow-xl flex flex-col md:flex-row justify-center lg:w-3/5 mx-auto items-center py-2 m-6 md:gap-4">
+        <img className="md:h-64 md:pb-5" src={image} alt="product image" />
         <div>
-          <h5 className="text-lg font-semibold text-green-400 rounded-sm px-[1px]">
+          <h5 className="text-3xl md:text-lg font-semibold text-green-400 rounded-sm px-[1px]">
             {brand_name}
           </h5>
-          <h5 className="text-xl font-semibold">{phone_name}</h5>
+          <h5 className="text-4xl md:text-xl font-semibold">{phone_name}</h5>
           <div>
-            <button onClick={handleFavoriteButton} className="btn bg-green-300 font-semibold my-2">
+            <button onClick={handleFavoriteButton} className="btn bg-green-300 font-semibold my-5 md:my-2">
               Add To Favorite
             </button>
           </div>
